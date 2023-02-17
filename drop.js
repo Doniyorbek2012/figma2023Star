49b550fc2326c07d853905cfc9714d162af0da82
Doniyorbek2012 <125459220+Doniyorbek2012@users.noreply.github.com>
2023-02-17 16:05:03 +0500

    Add files via upload

---
+++ drop.js
@@ -0,0 +1,9 @@
+
+let burger = document.getElementById("Burgers")
+burger.addEventListener("click", (e) => {
+    document.getElementById("naxx").style.left = `0`
+})
+let cLOSed = document.getElementById("frank")
+function Closs() {
+    document.getElementById("naxx").style.left = `-300px`
+}
